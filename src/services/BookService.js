4cccb16de9da2ae99f2c586e43105758168b7a45
danisonediel <edison.shano@hotmail.com>
2024-05-07 20:22:40 -0500

    bookservice generate domain greetings

--- src/services/BookService.js
+++ src/services/BookService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 //const BOOK_BASE_REST_API_URL ="https://rest-api-book-production.up.railway.app/api/books";
-const BOOK_SERVICE_LOCALHOST="http://localhost:8081/api/books";
-const BOOK_SERVICE_LOCALHOST_SAVE="http://localhost:8081/api/create";
+const BOOK_SERVICE_LOCALHOST="https://rest-api-book-production-2f54.up.railway.app/api/books";
+const BOOK_SERVICE_LOCALHOST_SAVE="https://rest-api-book-production-2f54.up.railway.app/api/create";
 class BookService {
     getAllBooks(){
         return axios.get(BOOK_SERVICE_LOCALHOST);
